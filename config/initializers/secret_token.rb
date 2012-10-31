00108d3f1db624c60aa206f0ab262d7b872bbc64
Mike MacDonald <empirenine@gmail.com>
2012-10-31 00:15:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetterpressCheats::Application.config.secret_token = '8ff1b3eb919949c38dd778a191a3b329c022acc0a9fb416988a8cd6f50e469d3d6ee8be822b630cd8bbb1d66965f59a652f7456812f9e584ab0cdf6519682946'
